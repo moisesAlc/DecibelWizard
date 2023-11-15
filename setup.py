0f5a499b4a16f56817d3124e488edcc73dd9a226
Moisés Madeira <msalcmd@gmail.com>
2023-11-15 09:18:28 -0300

    Added setup.py & requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DecibelWizard',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Moisés Madeira',
+    author_email='msalcmd@gmail.com',
+    description=''
+)
