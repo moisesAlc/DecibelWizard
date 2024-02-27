d48bb4c5c6895f056859721c86481c22faa9b7f5
Moisés Madeira <msalcmd@gmail.com>
2024-02-27 00:08:03 -0300

    refactor import and arg names

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from moviepy.editor import *
 
-import utils.guitools
+from gui import gui_utils
 
 
-def increase_volume_in_section(video_path, start_time, end_time, volume_multiplier, output_path):
+def increase_volume_in_section(video_path_arg, start_time_arg, end_time_arg, volume_multiplier_arg, output_path_arg):
     # Carrega o vídeo
-    video = VideoFileClip(video_path)
+    video = VideoFileClip(video_path_arg)
 
     # Define o trecho do vídeo em que o volume será aumentado
-    video_section = video.subclip(start_time, end_time)
+    video_section = video.subclip(start_time_arg, end_time_arg)
 
     # Aumenta o volume do trecho do vídeo
-    video_section = video_section.volumex(volume_multiplier)
+    video_section = video_section.volumex(volume_multiplier_arg)
 
     # Obtém o trecho do vídeo antes do ponto de início
-    pre_section = video.subclip(0, start_time)
+    pre_section = video.subclip(0, start_time_arg)
 
     # Obtém o trecho do vídeo após o ponto final
-    post_section = video.subclip(end_time, video.duration)
+    post_section = video.subclip(end_time_arg, video.duration)
 
     # Concatena os trechos do vídeo
     final_video = concatenate_videoclips([pre_section, video_section, post_section])
@@ -27,7 +27,7 @@ def increase_volume_in_section(video_path, start_time, end_time, volume_multipli
     final_video = final_video.set_duration(video.duration)
 
     # Salva o vídeo resultante com alta qualidade usando o codec libx264
-    final_video.write_videofile(output_path, codec='libx264', bitrate="5000k")
+    final_video.write_videofile(output_path_arg, codec='libx264', bitrate="5000k")
 
     # Fecha o vídeo carregado
     video.close()
@@ -36,12 +36,12 @@ def increase_volume_in_section(video_path, start_time, end_time, volume_multipli
 
 if __name__ == '__main__':
     # Exemplo de uso
-    video_path2 = utils.guitools.get_file_gui()
+    video_path2 = gui_utils.get_file_gui()
     video_path = r'C:\Users\MoisesMadeira\Downloads\Vídeo 00 - Sumário.mp4'
     start_time = 6  # Segundos - ponto de início da seção que terá o volume aumentado
     end_time = 127  # Segundos - ponto final da seção que terá o volume aumentado
     volume_multiplier = 5  # Fator pelo qual o volume será multiplicado (1.0 mantém o volume original)
-    output_path = utils.guitools.get_save_destination_gui()
+    output_path = gui_utils.get_save_destination_gui()
     output_path_filename = output_path.name
 
     increase_volume_in_section(
